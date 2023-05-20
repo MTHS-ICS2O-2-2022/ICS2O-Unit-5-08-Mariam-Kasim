fd1a9299b761cb56d0e23010aac4d7797218cdcc
Mariam-Kasim <124389357+Mariam-Kasim@users.noreply.github.com>
2023-05-20 20:51:10 +0000

    main

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Mariam Kasim
-// Created on: Mar 2023
+// Created on: May 2023
 // This file contains the JS functions for index.html
 
 "use strict"
